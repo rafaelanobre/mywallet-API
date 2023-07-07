0f967db504e69784c8cbd7bb853f76c1f69fc291
rafaelanobre <raafanobre@gmail.com>
2023-07-06 22:35:38 -0300

    Feat: signin and signup post

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 const app = express();
 app.use(cors());
@@ -20,4 +22,61 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
 export const db = mongoClient.db()
 
-app.listen(5000);
+const usuarioSchema = joi.object({
+    nome: joi.string().required(),
+    email: joi.string().email().required(),
+    senha: joi.string().required(),
+});
+
+app.post("/cadastro", async (req,res)=>{
+    const { nome, email, senha } = req.body;
+
+    const validation = usuarioSchema.validate(req.body, { abortEarly: false });
+
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try {
+        const usuario = await db.collection("users").findOne({ email });
+        if (usuario){
+            return res.status(409).send("E-mail já cadastrado");
+        }
+
+        const hash = bcrypt.hashSync(senha, 10);
+
+        await db.collection("users").insertOne({ nome, email, senha: hash });
+        res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.post("/", async (req,res)=>{
+    const { email, senha } = req.body;
+
+    try {
+        const usuario = await db.collection("users").findOne({ email });
+        if (!usuario){
+            return res.status(404).send("Usuário não cadastrado");
+        }
+
+        const acertouSenha = bcrypt.compareSync(senha, usuario.senha);
+        if (!acertouSenha){
+            return res.status(401).send("Senha incorreta");
+        }
+
+        await db.collection("session").deleteMany({ idUsuario: usuario._id });
+        const token = uuid();
+        await db.collection("session").insertOne({ token, idUsuario: usuario._id });
+
+        res.send(token);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+const port = process.env.PORT;
+
+app.listen(port);
